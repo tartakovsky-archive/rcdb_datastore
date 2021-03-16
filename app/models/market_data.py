32eacd24dc75e328c721f1f85cea52045d7c3171
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2021-03-16 09:40:17 +0300

    added dummy PKs

--- app/models/market_data.py
+++ app/models/market_data.py
@@ -1,5 +1,5 @@
 import datetime
-from sqlalchemy import Column, DateTime, Numeric, String, Enum, text
+from sqlalchemy import Column, DateTime, Numeric, String, Enum, text, BigInteger, Sequence
 
 from .base import Model
 from app.enums import Instrument
@@ -7,16 +7,19 @@ from app.enums import Instrument
 
 class MarketData(Model):
     __tablename__ = 'markets_data'
+    id_seq = Sequence(f'{__tablename__}_seq')
+
+    id = Column(BigInteger, id_seq, primary_key=True, server_default=id_seq.next_value())  # dummy pk
 
     timestamp = Column(
         DateTime,
         default=datetime.datetime.utcnow,
         server_default=text('now()'),
-        primary_key=True
+        nullable=False
     )
-    exchange = Column(String(16), primary_key=True)
-    symbol = Column(String(16), primary_key=True)
-    instrument = Column(Enum(Instrument, native_enum=False), index=True, primary_key=True)
+    exchange = Column(String(16), nullable=False, index=True)
+    symbol = Column(String(16), nullable=False, index=True)
+    instrument = Column(Enum(Instrument, native_enum=False), nullable=False, index=True)
     open = Column(Numeric(22, 10, asdecimal=False), nullable=False)
     high = Column(Numeric(22, 10, asdecimal=False), nullable=False)
     low = Column(Numeric(22, 10, asdecimal=False), nullable=False)
--- app/models/price_index.py
+++ app/models/price_index.py
@@ -1,17 +1,20 @@
 import datetime
-from sqlalchemy import Column, DateTime, Numeric, String, text
+from sqlalchemy import Column, DateTime, Numeric, String, text, Sequence, BigInteger
 
 from .base import Model
 
 
 class PriceIndex(Model):
     __tablename__ = 'price_indexes'
+    id_seq = Sequence(f'{__tablename__}_seq')
+
+    id = Column(BigInteger, id_seq, primary_key=True, server_default=id_seq.next_value())  # dummy pk
 
     timestamp = Column(
         DateTime,
         default=datetime.datetime.utcnow,
         server_default=text('now()'),
-        primary_key=True
+        nullable=False
     )
-    symbol = Column(String(16), primary_key=True)
+    symbol = Column(String(16), nullable=False, index=True)
     price = Column(Numeric(10, 8, asdecimal=False), nullable=False)
