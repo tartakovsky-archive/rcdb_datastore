c7f707b51489420bf08f1e76148d1c99b3047897
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2021-05-25 19:36:38 +0300

    fixed report query

--- app/views/rebate_report.sql
+++ app/views/rebate_report.sql
@@ -1,84 +1,123 @@
+WITH expected_rebates AS (
+    SELECT
+        trades.*, volume * 0.00005 as expected_rebate, volume_usd * 0.00005 as expected_rebate_usd
+    FROM (
+        SELECT
+            time_bucket('1 hour'::interval, timestamp) as ts,
+            name,
+            CASE
+                WHEN symbol LIKE 'EUR/%' OR symbol LIKE '%/EUR' THEN 'EUR'
+                WHEN symbol LIKE 'GBP/%' OR symbol LIKE '%/GBP' THEN 'GBP'
+                WHEN symbol LIKE 'AUD/%' OR symbol LIKE '%/AUD' THEN 'AUD'
+                WHEN symbol LIKE 'BRL/%' OR symbol LIKE '%/BRL' THEN 'BRL'
+                WHEN symbol LIKE 'TRY/%' OR symbol LIKE '%/TRY' THEN 'TRY'
+                WHEN symbol LIKE 'RUB/%' OR symbol LIKE '%/RUB' THEN 'RUB'
+                WHEN symbol LIKE 'UAH/%' OR symbol LIKE '%/UAH' THEN 'UAH'
+                ELSE 'NON_FIAT'
+            END as sym,
+            account_type,
+            SUM(
+                CASE
+                    WHEN symbol ilike ANY (
+                        select s || '%'
+                        from unnest(ARRAY ['EUR', 'GBP', 'AUD', 'BRL', 'TRY', 'RUB', 'UAH']::text[]) s(s))
+                THEN
+                    (CASE WHEN price_avg_buy = 0 THEN 0.0 ELSE volume_buy / price_avg_buy END) +
+                    (CASE WHEN price_avg_sell = 0 THEN 0.0 ELSE volume_sell / price_avg_sell END)
+                ELSE volume_buy + account_trades.volume_sell
+                END
+            ) as volume,
+            SUM(volume_buy_usd + volume_sell_usd) as volume_usd
+        FROM account_trades
+        WHERE timestamp >= :start_datetime
+          AND timestamp <= :end_datetime
+          AND (:param_account IS NULL OR :param_account = name || '_' || account_type)
+          AND name || '_' || account_type NOT IN :excluded_accounts
+        GROUP BY ts, name, sym, account_type
+    ) as trades
+    WHERE trades.sym in :currencies
+),
+recieved_rebates AS (
+    SELECT
+        *
+    FROM rebates
+    WHERE rebates.timestamp >= :start_datetime
+      AND rebates.timestamp <= :end_datetime
+      AND rebates.symbol IN :currencies
+      AND (:param_account IS NULL OR :param_account = rebates.name || '_' || rebates.account_type)
+      AND (rebates.name || '_' || rebates.account_type) NOT IN :excluded_accounts
+),
+merged_rebates AS (
+    SELECT
+        recieved_rebates.timestamp as rec_timestamp,
+        expected_rebates.ts as exp_timestamp,
+        recieved_rebates.symbol as rec_symbol,
+        expected_rebates.sym as exp_symbol,
+        recieved_rebates.account_type as rec_account_type,
+        expected_rebates.account_type as exp_account_type,
+        recieved_rebates.name as rec_name,
+        expected_rebates.name as exp_name,
+        expected_rebates.volume as volume,
+        expected_rebates.volume_usd as volume_usd,
+        recieved_rebates.rebate as rebate,
+        recieved_rebates.rebate_usd as rebate_usd,
+        expected_rebates.expected_rebate as expected_rebate,
+        expected_rebates.expected_rebate_usd as expected_rebate_usd
+    FROM recieved_rebates
+    LEFT JOIN expected_rebates ON recieved_rebates.timestamp = expected_rebates.ts
+                              AND recieved_rebates.symbol = expected_rebates.sym
+                              AND recieved_rebates.account_type = expected_rebates.account_type
+                              AND recieved_rebates.name = expected_rebates.name
+    UNION ALL
+    SELECT
+        recieved_rebates.timestamp as rec_timestamp,
+        expected_rebates.ts as exp_timestamp,
+        recieved_rebates.symbol as rec_symbol,
+        expected_rebates.sym as exp_symbol,
+        recieved_rebates.account_type as rec_account_type,
+        expected_rebates.account_type as exp_account_type,
+        recieved_rebates.name as rec_name,
+        expected_rebates.name as exp_name,
+        expected_rebates.volume as volume,
+        expected_rebates.volume_usd as volume_usd,
+        recieved_rebates.rebate as rebate,
+        recieved_rebates.rebate_usd as rebate_usd,
+        expected_rebates.expected_rebate as expected_rebate,
+        expected_rebates.expected_rebate_usd as expected_rebate_usd
+    FROM recieved_rebates
+    RIGHT JOIN expected_rebates ON recieved_rebates.timestamp = expected_rebates.ts
+                               AND recieved_rebates.symbol = expected_rebates.sym
+                               AND recieved_rebates.account_type = expected_rebates.account_type
+                               AND recieved_rebates.name = expected_rebates.name
+    WHERE recieved_rebates.timestamp IS NULL
+)
 SELECT
     DATE_TRUNC(
         :timeframe,
         CASE
-            WHEN expected_rebates.ts is NULL
-                THEN rebates.timestamp
-            ELSE expected_rebates.ts
+            WHEN rec_timestamp is NULL THEN exp_timestamp
+            ELSE rec_timestamp
         END
     ) as report_timestamp,
     CASE
-        WHEN expected_rebates.name is NULL THEN rebates.name
-        ELSE expected_rebates.name
+        WHEN rec_name is NULL THEN exp_name
+        ELSE rec_name
     END as report_name,
     CASE
-        WHEN expected_rebates.sym is NULL THEN rebates.symbol
-        ELSE expected_rebates.sym
+        WHEN rec_symbol is NULL THEN exp_symbol
+        ELSE rec_symbol
     END as report_symbol,
     CASE
-        WHEN expected_rebates.account_type is NULL THEN rebates.account_type
-        ELSE expected_rebates.account_type
+        WHEN rec_account_type is NULL THEN exp_account_type
+        ELSE rec_account_type
     END as report_account_type,
     SUM(COALESCE(volume, 0.0)) as volume,
     SUM(COALESCE(volume_usd, 0.0)) as volume_usd,
-    SUM(COALESCE(expected_rebates.expected_rebate, 0.0)) as report_expected_rebate,
-    SUM(COALESCE(rebates.rebate, 0.0)) as report_rebate,
-    SUM(COALESCE(expected_rebates.expected_rebate_usd, 0.0)) as report_expected_rebate_usd,
-    SUM(COALESCE(rebates.rebate_usd, 0.0)) as report_rebate_usd,
-    SUM(COALESCE(rebates.rebate, 0.0)) - SUM(COALESCE(expected_rebates.expected_rebate, 0.0)) as report_difference,
-    SUM(COALESCE(rebates.rebate_usd, 0.0)) - SUM(COALESCE(expected_rebates.expected_rebate_usd, 0.0)) as report_difference_usd
-FROM rebates
-    FULL OUTER JOIN (
-        SELECT
-            trades.*, volume * 0.00005 as expected_rebate, volume_usd * 0.00005 as expected_rebate_usd
-        FROM (
-            SELECT
-                time_bucket('1 hour'::interval, timestamp) as ts,
-                name,
-                sym,
-                account_type,
-                SUM(
-                    CASE
-                        WHEN symbol ilike ANY (
-                            select s || '%'
-                            from unnest(ARRAY ['EUR', 'GBP', 'AUD', 'BRL', 'TRY', 'RUB', 'UAH']::text[]) s(s))
-                        THEN
-                            (CASE WHEN price_avg_buy = 0 THEN 0.0 ELSE volume_buy / price_avg_buy END) +
-                            (CASE WHEN price_avg_sell = 0 THEN 0.0 ELSE volume_sell / price_avg_sell END)
-                        ELSE volume_buy + volume_sell
-                    END
-                ) as volume,
-                SUM(volume_buy_usd + volume_sell_usd) as volume_usd
-            FROM (
-                SELECT
-                    *,
-                    (CASE
-                        WHEN symbol LIKE 'EUR/%' OR symbol LIKE '%/EUR' THEN 'EUR'
-                        WHEN symbol LIKE 'GBP/%' OR symbol LIKE '%/GBP' THEN 'GBP'
-                        WHEN symbol LIKE 'AUD/%' OR symbol LIKE '%/AUD' THEN 'AUD'
-                        WHEN symbol LIKE 'BRL/%' OR symbol LIKE '%/BRL' THEN 'BRL'
-                        WHEN symbol LIKE 'TRY/%' OR symbol LIKE '%/TRY' THEN 'TRY'
-                        WHEN symbol LIKE 'RUB/%' OR symbol LIKE '%/RUB' THEN 'RUB'
-                        WHEN symbol LIKE 'UAH/%' OR symbol LIKE '%/UAH' THEN 'UAH'
-                        ELSE 'NON_FIAT'
-                    END) as sym
-                FROM db.public.account_trades
-                WHERE timestamp >= :start_datetime
-                  AND timestamp <= :end_datetime
-                  AND (:param_account IS NULL OR :param_account = name || '_' || account_type)
-                  AND name || '_' || account_type NOT IN :excluded_accounts
-            ) as account_tr
-            WHERE sym in :currencies
-            GROUP BY ts, name, sym, account_type
-        ) as trades
-        WHERE trades.sym <> 'NON_FIAT'
-    ) as expected_rebates ON rebates.timestamp = expected_rebates.ts
-                         AND rebates.symbol = expected_rebates.sym
-                         AND rebates.account_type = expected_rebates.account_type
-                         AND rebates.name = expected_rebates.name
-WHERE rebates.timestamp >= :start_datetime
-  AND rebates.timestamp <= :end_datetime
-  AND rebates.symbol IN :currencies
-  AND (:param_account IS NULL OR :param_account = rebates.name || '_' || rebates.account_type)
-  AND (rebates.name || '_' || rebates.account_type) NOT IN :excluded_accounts
-GROUP BY report_timestamp, report_name, report_account_type, report_symbol
+    SUM(COALESCE(expected_rebate, 0.0)) as report_expected_rebate,
+    SUM(COALESCE(rebate, 0.0)) as report_rebate,
+    SUM(COALESCE(expected_rebate_usd, 0.0)) as report_expected_rebate_usd,
+    SUM(COALESCE(rebate_usd, 0.0)) as report_rebate_usd,
+    SUM(COALESCE(rebate, 0.0)) - SUM(COALESCE(expected_rebate, 0.0)) as report_difference,
+    SUM(COALESCE(rebate_usd, 0.0)) - SUM(COALESCE(expected_rebate_usd, 0.0)) as report_difference_usd
+FROM merged_rebates
+GROUP BY report_timestamp, report_name, report_account_type, report_symbol;
